56f314d1834280e54418a955c40965dd79b7af15
Marcin Szymkowski <Marcin.Szymkowski@fabrity.pl>
2020-01-13 18:35:45 +0100

    Zaimplementowanie podstawowej logiki ONP - blad z nawiasami

--- ONP/Lab.cs
+++ ONP/Lab.cs
@@ -1,33 +1,86 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace ONP
 {
     class Lab
     {
+        public static readonly Dictionary<char, int> OperatorPriority = new Dictionary<char, int>() {
+            { '+', 1 },
+            { '-', 1 },
+            { '*', 2 },
+            { '/', 2 },
+            { '(', 3 }, // dodany sztucznie na potrzeby sprawdzenia ponizej
+            { ')', 3 },
+            { '|', 0 },
+        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Lingiwstyka matematyczna zadanie 4 - ONP");
+            Console.WriteLine("Lingwistyka matematyczna zadanie 4 - ONP");
             Console.WriteLine("----------------------------------------");
 
             // var input = "((2*5+1)/2)";
-            var input = "(2+1)*3-4*(7+4)";
+            var input = "(2+1)*3-4*(7+4)"; // wynik: 2 1 + 3 * 4 7 4 + * -
 
-            var stack = new Stack((uint)input.Length);
+            var stack = new Stack(input.Length);
 
             var output = "";
 
             foreach(var character in input)
             {
-
-                // nawias otwierajacy jest specjalnym przypadkiem - musi byc odlozony na dno stosu
-                if (character == '(')
+                if (Char.IsDigit(character))
                 {
-                    stack.pushToBottom(character);
+                    output += character;
+                }
+                else if(character == '(')
+                {
+                    stack.Push(character);
+                }
+                else if (character == ')')
+                {
+                    while(!stack.IsEmpty())
+                    {
+                        var popped = stack.Pop();
+
+                        // nawias otwierajacy jest specjalnym przypadkiem - jest traktowany jako dno stosu
+                        if (popped == '(')
+                        {
+                            break;
+                        }
+
+                        output += popped;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(character + " : " + output + " stack: " + stack.head);
+
+                    if (OperatorPriority[character] > OperatorPriority[stack.Peek()])
+                    {
+                        while (!stack.IsEmpty())
+                        {
+                            var popped = stack.Pop();
+
+                            // nawias otwierajacy jest specjalnym przypadkiem - jest traktowany jako dno stosu
+                            if (popped == '(')
+                            {
+                                break;
+                            }
+
+                            output += popped;
+                        }
+                    } else
+                    {
+                        stack.Push(character);
+                    }
                 }
             }
 
             Console.WriteLine(output);
+            Console.WriteLine("powinno byc");
+            Console.WriteLine("21+3*474+*-"); // TODO: co z liczbami ktore maja kilka cyfr?
 
             // pauzuje program
             Console.ReadLine();
--- ONP/Stack.cs
+++ ONP/Stack.cs
@@ -4,41 +4,66 @@ namespace ONP
 {
     class Stack
     {
-        private uint head;
+        private const int stackBottom = -1;
+
+        public int head; // wskazuje na szczytowy element stosu
         private char[] elements;
 
-        public Stack(uint length)
+        public Stack(int maxLength)
         {
-            elements = new char[length];
-            head = 0;
+            elements = new char[maxLength];
+
+            head = stackBottom;
         }
 
-        public void push(char element)
+        public void Push(char element)
         {
-            elements[head] = element;
-            head++;
+            elements[++head] = element;
         }
 
-        // specjalna metoda potrzebna do konwersji 
-        // na ONP - algorytm wymaga mozliwosci 
-        // dodania elementu na dol stosu
-        public void pushToBottom(char element)
-        {
-            // z natury zadania wynika ze nie ma ryzyka
-            // ze element taki pojawi sie na koncu stosu - parametrem
-            // w praktyce moze byc tylko nawias otwierajacy
-            for (uint i = head; i > 0; i--)
-            {
-                elements[i + 1] = elements[i];
-            }
+        //// specjalna metoda potrzebna do konwersji 
+        //// na ONP - algorytm wymaga mozliwosci 
+        //// dodania elementu na dol stosu
+        //public void pushToBottom(char element)
+        //{
+        //    // z natury zadania wynika ze nie ma ryzyka
+        //    // ze element taki pojawi sie na koncu stosu - parametrem
+        //    // w praktyce moze byc tylko nawias otwierajacy
+        //    for (uint i = head; i > 0; i--)
+        //    {
+        //        elements[i + 1] = elements[i];
+        //    }
 
-            elements[0] = element;
-            head++;
+        //    elements[0] = element;
+
+        //    head++;
+        //}
+
+        public char Pop()
+        {
+            return elements[head--];
         }
 
-        public char pop()
+        public char Peek()
         {
-            return elements[--head];
+            return this.IsEmpty()? '|' : elements[head];
         }
+
+        public bool IsEmpty()
+        {
+            return (head == stackBottom);
+        }
+
+        //public char[] popAll()
+        //{
+        //    var result = new char[head];
+
+        //    do
+        //    {
+        //        result[maxLength - head] += this.Pop();
+        //    } while (head > 0);
+
+        //    return result;
+        //}
     }
 }
