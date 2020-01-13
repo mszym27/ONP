d7064617a7ac77ba5f057454303f2be6e69e496f
Marcin Szymkowski <Marcin.Szymkowski@fabrity.pl>
2020-01-13 12:27:38 +0100

    Add project files.

---
+++ ONP/Lab.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace ONP
+{
+    class Lab
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lingiwstyka matematyczna zadanie 4 - ONP");
+            Console.WriteLine("----------------------------------------");
+
+            // var input = "((2*5+1)/2)";
+            var input = "(2+1)*3-4*(7+4)";
+
+            var stack = new Stack((uint)input.Length);
+
+            var output = "";
+
+            foreach(var character in input)
+            {
+
+                // nawias otwierajacy jest specjalnym przypadkiem - musi byc odlozony na dno stosu
+                if (character == '(')
+                {
+                    stack.pushToBottom(character);
+                }
+            }
+
+            Console.WriteLine(output);
+
+            // pauzuje program
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ONP/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ONP
+{
+    class Stack
+    {
+        private uint head;
+        private char[] elements;
+
+        public Stack(uint length)
+        {
+            elements = new char[length];
+            head = 0;
+        }
+
+        public void push(char element)
+        {
+            elements[head] = element;
+            head++;
+        }
+
+        // specjalna metoda potrzebna do konwersji 
+        // na ONP - algorytm wymaga mozliwosci 
+        // dodania elementu na dol stosu
+        public void pushToBottom(char element)
+        {
+            // z natury zadania wynika ze nie ma ryzyka
+            // ze element taki pojawi sie na koncu stosu - parametrem
+            // w praktyce moze byc tylko nawias otwierajacy
+            for (uint i = head; i > 0; i--)
+            {
+                elements[i + 1] = elements[i];
+            }
+
+            elements[0] = element;
+            head++;
+        }
+
+        public char pop()
+        {
+            return elements[--head];
+        }
+    }
+}
