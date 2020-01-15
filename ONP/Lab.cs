143b3606aac946d4487eff1b577d296f811cbb80
Marcin Szymkowski <Marcin.Szymkowski@fabrity.pl>
2020-01-15 17:12:02 +0100

    Uwzględnienie liczb kilkucyfrowych w wejściu

--- ONP/Lab.cs
+++ ONP/Lab.cs
@@ -11,19 +11,50 @@ namespace ONP
             Console.WriteLine("Lingwistyka matematyczna zadanie 4 - ONP");
             Console.WriteLine("----------------------------------------");
 
-            var input = "((2*5+1)/2)";
+            var input = "((20*50+10)/20)";
             //  var input = "(2+1)*3-4*(7+4)"; // wynik: 2 1 + 3 * 4 7 4 + * -
             //Console.WriteLine("powinno byc" + "21+3*474+*-"); // TODO: co z liczbami ktore maja kilka cyfr?
 
-            var stack = new ONPStack(input.Length);
+            var inputLength = input.Length;
+
+            var stack = new ONPStack(inputLength);
 
             var output = "";
 
-            foreach (var character in input)
+            for (int i = 0; i < inputLength; i++)
             {
+                var character = input[i];
+
                 if (Char.IsDigit(character))
                 {
-                    output += character;
+                    // program uwzglednia mozliwosc 
+                    // pojawienia sie na wejsciu
+                    // liczby kilkucyfrowej
+                    // sprawdzane sa nastepne znaki
+                    // po napotkaniu cyfr uznaje sie je za czesc liczby
+                    var j = i + 1;
+
+                    var wholeNumberToConcat = "" + character;
+
+                    for (; j < inputLength; j++)
+                    {
+                        character = input[j];
+
+                        if (Char.IsDigit(character))
+                        {
+                            wholeNumberToConcat += character;
+                        }
+                        else
+                        {
+                            j--;
+
+                            break;
+                        }
+                    }
+
+                    i = j;
+
+                    output += wholeNumberToConcat;
                 }
                 else if (character == '(')
                 {
